8d9c6553dad61d2aac12be071bc0d284885a307e
Eric Lemieux <eric@lemieuxdev.com>
2021-01-19 22:27:06 -0500

    Make main script compatible with sh

--- src/jack-the-ripper.sh
+++ src/jack-the-ripper.sh
@@ -1,30 +1,30 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
 # Determine if a DVD or BluRay has been inserted into the disc drive, and if it has rip the content into a location for
 # further processing.
 
-set -euo pipefail
+set -eu
 
 LOG_FILE="/var/log/jack-the-ripper.log"
 INGEST_DIRECTORY="/mnt/vault/Ingest"
 
 # Logging utilities
-function log::_timestamp() { date +'%Y-%m-%dT%H:%M:%S%z'; }
-function log::info() { echo "$(log::_timestamp) [INFO] $*" >> "${LOG_FILE}"; }
-function log::warn() { echo "$(log::_timestamp) [WARN] $*" >> "${LOG_FILE}"; }
-function log::error() { echo "$(log::_timestamp) [ERROR] $*" >> "${LOG_FILE}"; }
+log_timestamp() { date +'%Y-%m-%dT%H:%M:%S%z'; }
+log_info() { echo "$(log_timestamp) [INFO] $*" >> "${LOG_FILE}"; }
+log_warn() { echo "$(log_timestamp) [WARN] $*" >> "${LOG_FILE}"; }
+log_error() { echo "$(log_timestamp) [ERROR] $*" >> "${LOG_FILE}"; }
 
 # Rip the disc in order to archive the contents
-function rip_disc() {
-  local disc_name="${ID_FS_LABEL:-}"
+rip_disc() {
+  disc_name="${ID_FS_LABEL:-}"
 
-  if [[ -z "${disc_name}" ]]; then
-    log::error "Disc name was detected as empty, a human needs to investigate"
+  if [ -z "${disc_name}" ]; then
+    log_error "Disc name was detected as empty, a human needs to investigate"
     exit 1
   fi
 
-  local working_dir="${INGEST_DIRECTORY}/${disc_name}"
-  log::info "Detected name of the disc is ${disc_name} will save to ${working_dir}"
+  working_dir="${INGEST_DIRECTORY}/${disc_name}"
+  log_info "Detected name of the disc is ${disc_name} will save to ${working_dir}"
 
   mkdir -p "${working_dir}"
   makemkvcon --robot mkv disc:0 all "${working_dir}" >> "${LOG_FILE}" 2>&1
@@ -37,18 +37,18 @@ function rip_disc() {
 }
 
 # Main entry point
-function main() {
-  log::info "Starting to process disc drive event"
+main() {
+  log_info "Starting to process disc drive event"
 
-  local dvd="${ID_CDROM_MEDIA_DVD:-}"
-  local bluray="${ID_CDROM_MEDIA_BD:-}"
+  dvd="${ID_CDROM_MEDIA_DVD:-}"
+  bluray="${ID_CDROM_MEDIA_BD:-}"
 
-  if [[ "${bluray}" -eq "1" || "${dvd}" -eq "1" ]]; then
-    log::info "It's a dvd or bluray, we should archive it"
+  if [ "${bluray}" -eq "1" ] || [ "${dvd}" -eq "1" ]; then
+    log_info "It's a dvd or bluray, we should archive it"
     rip_disc
-    log::info "Completed ripping the disc"
+    log_info "Completed ripping the disc"
   else
-    log::info "It's not a DVD or a BluRay, skipping processing"
+    log_info "It's not a DVD or a BluRay, skipping processing"
     exit 0;
   fi
 }

2c81305eaaaa314cdbf2eab161fd07b74b977dd3
Eric Lemieux <eric@lemieuxdev.com>
2021-01-19 21:11:58 -0500

    Detecting if the disc is a dvd or bluray

--- src/jack-the-ripper.sh
+++ src/jack-the-ripper.sh
@@ -6,6 +6,7 @@
 set -euo pipefail
 
 LOG_FILE="/var/log/jack-the-ripper.log"
+INGEST_DIRECTORY="/mnt/vault/Ingest"
 
 # Logging utilities
 function log::_timestamp() { date +'%Y-%m-%dT%H:%M:%S%z'; }
@@ -13,17 +14,31 @@ function log::info() { echo "$(log::_timestamp) [INFO] $*" >> "${LOG_FILE}"; }
 function log::warn() { echo "$(log::_timestamp) [WARN] $*" >> "${LOG_FILE}"; }
 function log::error() { echo "$(log::_timestamp) [ERROR] $*" >> "${LOG_FILE}"; }
 
+# Rip the disc in order to archive the contents
+function rip_disc() {
+  local disc_name="${ID_FS_LABEL:-}"
+  local working_dir="${INGEST_DIRECTORY}/${disc_name}"
+  log::info "Detected name of the disc is ${disc_name} will save to ${working_dir}"
+
+  # TODO: Actually do the ripping
+
+  # TODO: Eject the disc
+}
+
 # Main entry point
 function main() {
-  log::info "Some information"
-  log::warn "Something bad happened"
-  log::error "Uh oh, something broke"
+  log::info "Starting to process disc drive event"
 
   local dvd="${ID_CDROM_MEDIA_DVD:-}"
   local bluray="${ID_CDROM_MEDIA_BD:-}"
-  local cd="${ID_CDROM_MEDIA_CD:-}"
-  log::info "BluRay: ${bluray}"
-  log::info "DVD: ${dvd}"
-  log::info "CD: ${cd}"
+
+  if [[ "${bluray}" -eq "1" || "${dvd}" -eq "1" ]]; then
+    log::info "It's a dvd or bluray, we should archive it"
+    rip_disc
+    log::info "Completed ripping the disc"
+  else
+    log::info "It's not a DVD or a BluRay, skipping processing"
+    exit 0;
+  fi
 }
 main "$@"
